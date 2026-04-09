47b13bfa05a785ea17cd2c3853af4fa1f20bfa9c
unknown <lakshtandon3@gmail.com>
2026-04-09 11:50:31 +0530

    laksh: preprocessing improved

--- main.py
+++ main.py
@@ -11,3 +11,17 @@ print(df.info())
 
 # Show statistics
 print(df.describe())
+
+# ========================
+# Data Preprocessing (Improved)
+# ========================
+
+# Use MEAN instead of median (improvement)
+df['Age'].fillna(df['Age'].mean(), inplace=True)
+df['Embarked'].fillna(df['Embarked'].mode()[0], inplace=True)
+
+df.drop('Cabin', axis=1, inplace=True)
+
+df['Sex'] = df['Sex'].map({'male': 0, 'female': 1})
+
+df = pd.get_dummies(df, columns=['Embarked'], drop_first=True)
